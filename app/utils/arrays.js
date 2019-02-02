4cb4cc6734da309a231cd38e32359a1cad8abdce
Zac Delventhal <delventhalz@gmail.com>
2019-02-02 00:42:43 -0600

    Add array utils for hex->byte conversion
    Meeba DNA will be read as Uint8Arrays and stored/displayed as
    hex strings.
    
    Signed-off-by: Zac Delventhal <delventhalz@gmail.com>

--- app/utils/arrays.js
+++ app/utils/arrays.js
@@ -4,5 +4,39 @@
  * @param {number} len - the length of the new array
  * @returns {number[]}
  */
-// eslint-disable-next-line import/prefer-default-export
 export const range = len => Array(...Array(len)).map((_, i) => i);
+
+/**
+ * Creates a new 2D array split into evenly sized chunks
+ *
+ * @param {array} arr - the length of the new array
+ * @param {number} size - the size of each chunk
+ * @returns {array[]}
+ */
+export const chunk = (arr, size) => range(Math.ceil(arr.length / 2))
+  .map(i => arr.slice(i * size, (i + 1) * size));
+
+/**
+ * Converts a hexadecimal string to a Uint8Array
+ *
+ * @param {string} hex - the hex string
+ * @returns {Uint8Array}
+ */
+export const toBytes = hex => Uint8Array.from(
+  chunk(hex.split(''), 2).map(byte => parseInt(byte.join(''), 16)),
+);
+
+/**
+ * Converts a Uiny8Array into an uppercase hexadecimal string
+ *
+ * @param {Uint8Array} bytes
+ * @returns {string}
+ */
+export const toHex = bytes => Array.prototype.map.call(
+  bytes,
+  /**
+   * @param {number} byte
+   * @returns {string}
+   */
+  byte => byte.toString(16).padStart(2, '0').toUpperCase(),
+).join('');
--- tests/app/utils/arrays.test.js
+++ tests/app/utils/arrays.test.js
@@ -1,7 +1,12 @@
 'use strict';
 
 const { expect } = require('chai');
-const { range } = require('./arrays.common.js');
+const {
+  range,
+  chunk,
+  toBytes,
+  toHex,
+} = require('./arrays.common.js');
 
 describe('Array utils', () => {
   describe('range', () => {
@@ -15,6 +20,44 @@ describe('Array utils', () => {
 
     it('should create an empty array when given a length of 0', () => {
       expect(range(0)).to.be.an('array').with.a.lengthOf(0);
+    });
+  });
+
+  describe('chunk', () => {
+    it('should create a new 2D array with evenly sized chunks', () => {
+      expect(chunk([1, 2, 3, 4], 2)).to.deep.equal([[1, 2], [3, 4]]);
+    });
+
+    it('should include any extra elements in the final chunk', () => {
+      expect(chunk([1, 2, 3, 4], 3)).to.deep.equal([[1, 2, 3], [4]]);
+    });
+  });
+
+  describe('toBytes', () => {
+    it('should create a Uint8Array from a hex string', () => {
+      const bytes = toBytes('FF00BA');
+
+      expect(bytes).to.be.an.instanceOf(Uint8Array).with.a.lengthOf(3);
+      expect(bytes).to.deep.equal(new Uint8Array([255, 0, 186]));
+    });
+
+    it('should create an empty Uint8Array from an empty string', () => {
+      const bytes = toBytes('');
+      expect(bytes).to.be.an.instanceOf(Uint8Array).with.a.lengthOf(0);
+    });
+  });
+
+  describe('toBytes', () => {
+    it('should create a hex string from a Uint8Array', () => {
+      const hex = toHex(new Uint8Array([255, 0, 186]));
+
+      expect(hex).to.be.a('string').with.a.lengthOf(6);
+      expect(hex).to.equal('FF00BA');
+    });
+
+    it('should create an empty Uint8Array from an empty string', () => {
+      const hex = toHex(new Uint8Array([]));
+      expect(hex).to.equal('');
     });
   });
 });
