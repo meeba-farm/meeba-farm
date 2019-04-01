7a39f2243254c6a999b60248f95bf0285663752b
Zac Delventhal <delventhalz@gmail.com>
2019-04-01 10:09:17 -0500

    Create global MeebaFarm object
    This will be used to store debug objects like bodies, as well as
    future methods DOM objects will call.
    
    Signed-off-by: Zac Delventhal <delventhalz@gmail.com>

--- app/main.js
+++ app/main.js
@@ -19,23 +19,22 @@ const { width, height } = settings.tank;
 const view = createView(width, height);
 const renderFrame = getFrameRenderer(view);
 
-let bodies = range(settings.simulation.bodies).map(getRandomBody);
-separateBodies(bodies);
-
-// Add bodies to window for debugging purposes (hack window type to allow this)
 /** @type {Object<string, any>} */
 const anyWindow = window;
-anyWindow.bodies = bodies;
+const MeebaFarm = {};
+anyWindow.MeebaFarm = MeebaFarm;
+
+MeebaFarm.bodies = range(settings.simulation.bodies).map(getRandomBody);
+separateBodies(MeebaFarm.bodies);
 
 /** @param {number} lastTick */
 const simulate = (lastTick) => {
   const thisTick = performance.now();
-  bodies = simulateFrame(bodies, lastTick, thisTick);
-  anyWindow.bodies = bodies;
+  MeebaFarm.bodies = simulateFrame(MeebaFarm.bodies, lastTick, thisTick);
   setTimeout(() => simulate(thisTick), 8);
 };
 const render = () => {
-  renderFrame(bodies);
+  renderFrame(MeebaFarm.bodies);
   requestAnimationFrame(render);
 };
 
